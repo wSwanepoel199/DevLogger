a727403260686c7a71f59e3728a2b9bd2088115e
wSwanepoel <61444827+wSwanepoel199@users.noreply.github.com>
2020-03-02 14:03:39 +1100

    Added stuff to Logs

--- DevLogger/src/app/components/logs/logs.component.ts
+++ DevLogger/src/app/components/logs/logs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GeneratedFile } from '@angular/compiler';
 
 @Component({
   selector: 'app-logs',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./logs.component.css']
 })
 export class LogsComponent implements OnInit {
+  logs: {
+    id: string,
+    text: string,
+    date: any
+  }[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.logs = [
+      {
+        id: '1', text: "GeneratedFile", date: new Date('12/26/2017 12:54:23')
+      },
+      {
+        id:'2', text: "Added Bootstrap", date: new Date('12/26/2017 12:54:23')
+      },
+      {
+        id:'3', text: "Screwed up Log-form", date: new Date('12/26/2017 12:54:23')
+      }
+    ]
   }
 
 }
